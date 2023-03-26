e42ede7ab7db974346d8e067234f5cfa5324f623
Dan Coffey <dcoffey90@gmail.com>
2023-03-26 12:13:53 +0100

    Created StackNavigator.js file

---
+++ StackNavigator.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
